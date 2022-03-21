758bf94395c026ae2ac0bdb1f3eaf64bf3a4728a
mprietoo <m.prietoo661@gmail.com>
2022-03-21 11:57:10 +0100

    non initial commit
    I don´t remember what is this...

--- Rumbaracha_ASTI2022_CodigoLaberinto.ino
+++ Rumbaracha_ASTI2022_CodigoLaberinto.ino
@@ -1,3 +1,10 @@
+//Cosas que hacer:
+/*
+f1 -> Arduino: Board config -> ESP32 Dev Module
+f1 -> Arduino: Initialize (solo una vez)
+f1 -> Arduino: Verify (compilar)
+*/
+
 #include "funcionesSetup.h"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -7,10 +14,20 @@
 #include "PCF8575.h"
 #include "Pinout.h"
 #include "Config.h"
+#include "Motor.h"
+
+ //Objeto global para el extensor de pines
+PCF8575 pinExtensor =  PCF8575(0x20);
 
 //objeto global para el display
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); 
 
+//Objeto que maneja los 3 sensores tof
+TofSensors MySensors(1, 2, 3, &pinExtensor); 
+
+//Objetos para los dos motores (NO control de posicion)
+Motor Motor_derecho(PIN_MOTOR_D_IN1, PIN_MOTOR_D_IN2, PIN_MOTOR_D_PWM, PWM_CH_D, PWM_FREC, PWM_RES); //Motor derecho
+Motor Motor_izquierdo(PIN_MOTOR_I_IN1, PIN_MOTOR_I_IN2, PIN_MOTOR_I_PWM, PWM_CH_I, PWM_FREC, PWM_RES); //Motor izquierdo
 
 void setup(){
 
